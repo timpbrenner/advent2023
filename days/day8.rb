a7478db75c221b62acd2fa1a7d79c8e119a52f4c
Tim Brenner <timpbrenner@gmail.com>
2023-12-09 08:35:22 -0500

    Daqy 8, supermessy

--- days/day8.rb
+++ days/day8.rb
@@ -6,31 +6,53 @@ module Days
     
       directions = lines.shift.chars
       lines.shift
-
       paths = parse_paths(lines)
-       
-      current = 'AAA'
+
+      z_steps = []
+      try_lcm=[]
+      paths.keys.each do |p|
+        next unless p.end_with?('A')
+
+        z_step = get_steps_for_start(p, paths, directions)
+        try_lcm << z_step[0]
+        get_step_info(z_step)
+        z_steps << z_step
+      end
+
+      puts "Final"
+      puts try_lcm.inspect
+      puts try_lcm.reduce(1) { |t, n| t.lcm(n) }
+    end
+
+    private
+
+
+    def self.get_step_info(z_steps)
+      puts z_steps[0]
+      (1..(z_steps.count - 1)).each do |z|
+        puts "#{z}:#{z_steps[z]} - #{z_steps[z] - z_steps[z - 1]}"
+      end
+    end
+
+    def self.get_steps_for_start(start, paths, directions)
+      z_steps = []
+      current = start
       step = 0
 
       loop do
-        break if current == 'ZZZ'
+        break if z_steps.count > 20
+        z_steps << step if current.end_with?('Z')
 
         index = step % (directions.count - 1)
-        puts "Index: #{index}"
         direction = directions[index]
         path = paths[current]
-
-        puts direction
-        puts path.inspect
 
         step += 1
         current = direction == 'R' ? path[1] : path[0]
       end
 
-      puts "Steps: #{step}"
+      z_steps
     end
-
-    private
 
     def self.parse_paths(lines)
       paths = {}

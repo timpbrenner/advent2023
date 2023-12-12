30c7b4ace6d1820b98cb8efa1cf44574eaed865e
Tim Brenner <timpbrenner@gmail.com>
2023-12-11 19:41:15 -0500

    ALl caught up

--- days/day11.rb
+++ days/day11.rb
@@ -9,20 +9,24 @@ module Days
       end
       puts '=' * 10
 
-      expanded = expand_grid(grid)
-      expanded.each do |row|
-        puts row.join('')
-      end
+      expansions = get_expansions(grid)
+      # expanded = expand_grid(grid)
+      # expanded.each do |row|
+      #   puts row.join('')
+      # end
+      puts expansions.inspect
 
-      galaxies = get_galaxies(expanded)
+      galaxies = get_galaxies(grid)
       puts galaxies.inspect
 
       total_distance = 0
       galaxies.each do |s|
         galaxies.each do |e|
           next if s[0] == e[0] && s[1] == e[1]
-          total_distance += get_distance(s,e)
-          puts "#{s.inspect} - #{e.inspect} = #{get_distance(s,e)}"
+          distance = get_distance(s, e, expansions)
+
+          total_distance += distance
+          puts "#{s.inspect} - #{e.inspect} = #{distance}"
         end
       end
 
@@ -32,8 +36,29 @@ module Days
 
     private
 
-    def self.get_distance(s, e)
-      (s[0] - e[0]).abs + (s[1] - e[1]).abs;
+    EXPANSION_METRIC = 1_000_000
+
+    def self.get_distance(s, e, expansions)
+      xs = [s[0], e[0]].sort
+      ys = [s[1], e[1]].sort
+
+      passing_blank_rows = 0
+      passing_blank_col = 0
+
+      expansions[0].each do |blank_row|
+        next unless (ys[0] < blank_row && blank_row < ys[1])
+
+        passing_blank_rows += EXPANSION_METRIC - 1
+      end
+
+      expansions[1].each do |blank_col|
+        next unless (xs[0] < blank_col && blank_col < xs[1])
+
+        passing_blank_col += EXPANSION_METRIC - 1
+      end
+
+      (s[0] - e[0]).abs + (s[1] - e[1]).abs + passing_blank_rows + passing_blank_col
+
     end
 
     def self.get_galaxies(grid)
@@ -47,6 +72,22 @@ module Days
       end 
 
       galaxies
+    end
+
+    def self.get_expansions(grid)
+      empty_rows = []
+      grid.each_with_index do |row, i|
+        next unless row.all? { |c| c == '.' }
+        empty_rows << i
+      end
+
+      empty_cols = []
+      (0..grid[0].count).each do |y|
+        next unless grid.all? { |row| row[y] == '.'}
+        empty_cols << y
+      end
+
+      [empty_rows, empty_cols]
     end
 
     def self.expand_grid(grid)
